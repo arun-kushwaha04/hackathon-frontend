a407b08d9f4008adbaad14c3a1218f4d6a563e26
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-09-18 15:24:34 +0530

    Added doctor cards

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -1,13 +1,93 @@
 import React from "react";
 import styled from "styled-components";
 import { UseGlobalContext } from "../../Context";
+import { doctor } from "../../data";
 import Filter from "../Filter/filter";
+import { Button } from "reactstrap";
 import "./dashboard.scss";
 
 export default function dashboard() {
   return (
     <section className="main-section-dashboard">
       <Filter />
+      <CardDiv>
+        {doctor.map((element, index) => {
+          return (
+            <div key={`card-element-${index}`}>
+              <h1>{element.specialization} Specialist</h1>
+              <article>
+                <p>
+                  <span>Doctor {element.name}</span>
+                </p>
+                <p>
+                  <span>Experience :</span> {element.experience}
+                </p>
+                <p>
+                  <span>Rating :</span> {element.rating}
+                </p>
+                <p>
+                  <span>Fees :</span> ₹{element.price}
+                </p>
+              </article>
+              <Button type="submit">Shedule Appointment</Button>
+            </div>
+          );
+        })}
+      </CardDiv>
     </section>
   );
 }
+
+const CardDiv = styled.div`
+  width: 95%;
+  margin: auto;
+  font-size: 1.5rem;
+  font-family: "Roboto", sans-serif;
+  height: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 25rem);
+  grid-row-gap: 2rem;
+  justify-content: space-evenly;
+  padding: 2rem 0;
+
+  > div {
+    width: 25rem;
+    height: 25rem;
+    margin: auto;
+    font-size: 1.5rem;
+    padding: 1rem;
+    background: var(--Lightest_Slate);
+    position:relative;
+    border-radius:1rem;
+    box-shadow: 0 2px 4px 0 rgba( 31, 38, 135, 0.37 );
+    h1 {
+      text-align: center;
+      color: var(--Lighest_Backround);
+      font-weight: 400;
+      width: auto;
+      margin: auto;
+      margin-bottom: 1rem;
+    }
+    button {
+     background-color: black:
+     color: white;
+     position:absolute;
+     bottom:2rem;
+     right:1rem;
+     outline:none;
+     border:none;
+     padding:1rem 0.5rem;
+     border-radius:0.5rem;
+     cursor:pointer;
+     font-family: 'Roboto', sans-serif;
+     left:1rem;
+     font-size:1.5rem;
+    }
+  }
+  article {
+    font-family: "Roboto Mono", monospace;
+    span {
+      font-weight: 500;
+    }
+   }  
+`;
--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,7 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [loginStatus, setLoginStatus] = useState(false);
-  const [userType, setUserType] = useState("Patient");
+  const [userType, setUserType] = useState("Doctor");
   const [filterArray, setFilterArray] = useState(["rating"]);
 
   return (
