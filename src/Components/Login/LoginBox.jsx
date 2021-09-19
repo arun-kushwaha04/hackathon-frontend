505bfda8861ec0ec185c003e0fc18288b2ded6d7
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-09-19 18:57:19 +0530

    Removed warning

--- src/Components/Login/LoginBox.jsx
+++ src/Components/Login/LoginBox.jsx
@@ -1,31 +1,62 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-import {UseGlobalContext} from '../../Context.js'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { UseGlobalContext } from "../../Context.js";
 
 function LoginBox() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const { setLoginStatus } = UseGlobalContext();
 
-    const [email,setEmail] = useState(null)
-    const [password,setPassword] = useState(null)
-    const {loginStatus,setLoginStatus} = UseGlobalContext()
+  const login = async () => {
+    setLoginStatus(true);
+  };
 
-    const login = async()=>{
-        setLoginStatus(true)
-    }
+  console.log(email, password);
 
-
-    return (
-        <>
-            <h1 id="login">LogIn</h1><br/>
-            <input type="email" id="email" class="client-info" required onChange={(e)=>{setEmail(e.target.value)}} />
-            <label for="email" className="label">Email</label>
-            <input type="password" id="password" class="client-info" required onChange={(e)=>{setPassword(e.target.value)}} />
-            <label for="password" className="label">Password</label>
-            <input type="button" id="submit" class="client-info" value="Submit" onClick={()=>{login()}}/>
-            <div>
-                <Link className="box-nav-link" to="/signup">Create an account</Link>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h1 id="login">LogIn</h1>
+      <br />
+      <input
+        type="email"
+        id="email"
+        class="client-info"
+        required
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <label for="email" className="label">
+        Email
+      </label>
+      <input
+        type="password"
+        id="password"
+        class="client-info"
+        required
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <label for="password" className="label">
+        Password
+      </label>
+      <input
+        type="button"
+        id="submit"
+        class="client-info"
+        value="Submit"
+        onClick={() => {
+          login();
+        }}
+      />
+      <div>
+        <Link className="box-nav-link" to="/signup">
+          Create an account
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default LoginBox
+export default LoginBox;
