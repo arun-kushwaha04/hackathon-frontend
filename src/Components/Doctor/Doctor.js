985c046c98670a113f46bab37fad01301fc81134
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-09-19 11:53:30 +0530

    Created doctor page and styled it

--- src/Components/Doctor/Doctor.js
+++ src/Components/Doctor/Doctor.js
@@ -1,6 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+import "./doctor.scss";
+
+const data = {
+  _id: "6145ba9d43d7b683e0de5280",
+  name: "Test",
+  email: "abcd@gmail.com",
+  contact: "9612014587",
+  experience: 1,
+  fees: 400,
+  specialization: "heart",
+  rating: 0,
+  totalRatings: 0,
+  about:
+    "I am very good doctor please come to me for your heart operation. I only charge 400 ruppes that is very less please come fast please.",
+  slot: ["10am-12pm", "2pm-3pm", "5pm-7pm"],
+};
 
 export default function Doctor() {
   const doctorId = window.location.pathname.split("/doctor/")[1];
-  return <h1> Hello from doctor {doctorId} </h1>;
+  const name = "Doctor " + data.name;
+  return (
+    <section className="main-section-for-each-page">
+      <MainDiv className="doctor-detail-list">
+        <h1> {name} </h1>
+        <h2> {data.specialization} </h2>
+        <br />
+        <br />
+        <p>{data.about}</p>
+        <br />
+        <p>
+          {name} has an experience of {data.experience} years.
+        </p>
+        <br />
+        <p>
+          {name} charges ₹{data.fees} for every appointment.
+        </p>
+        <br />
+        {data.rating > 0 && (
+          <>
+            <p>
+              {name} has got an rating of {data.rating}/5 by {data.totalRatings}{" "}
+              patients.
+            </p>
+            <br />
+          </>
+        )}
+        <p>Want To book A slot ??</p>
+        <br />
+        <div>
+          <p>{name} is Available between these times.</p>
+          <br />
+          <ul>
+            {data.slot.map((ele, index) => (
+              <li key={`timeslot-doctor-${index}`}>
+                <p>{ele}</p>
+                <br />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <br />
+      </MainDiv>
+    </section>
+  );
 }
+
+const MainDiv = styled.div`
+  width: 50%;
+  margin: auto;
+  font-size: 1.5rem;
+  font-family: "Roboto", sans-serif;
+  height: auto;
+  padding: 2rem 3rem;
+  background: #e5f6fd;
+  border-radius: 1rem;
+`;
